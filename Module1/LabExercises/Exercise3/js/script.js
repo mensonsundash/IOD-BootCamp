7ed84a913eb85fe667831aa403f4249ec38b3f11
menso431 <menson.sundash@gmail.com>
2025-10-15 18:46:34 +1100

    Lab Exercise3 - Extension 1&2

--- Module1/LabExercises/Exercise3/js/script.js
+++ Module1/LabExercises/Exercise3/js/script.js
@@ -1,11 +1,25 @@
 function changeMe1() {
     document.getElementById('column1').style.backgroundColor= "blue";
-    document.getElementById('heading1').innerText = "Hello Everyone";
+    let input1 = document.getElementById('input1').value.trim();
+    document.getElementById('heading1').innerText = input1;
     document.getElementById('heading1').style.color = "#ffffff";
 }
 
 function changeMe2() {
     document.getElementById('column2').style.backgroundColor= "red";
-    document.getElementById('heading2').innerText = "Welcome World";
+    let input2 = document.getElementById('input2').value.trim();
+    document.getElementById('heading2').innerText = input2;
     document.getElementById('heading2').style.color = "#ffffff";
+}
+
+function reset() {
+    document.getElementById('column1').style.backgroundColor= "";
+    document.getElementById('heading1').innerText = "Hello";
+    document.getElementById('heading1').style.color = "";
+    document.getElementById('input1').value = "";
+
+    document.getElementById('column2').style.backgroundColor= "";
+    document.getElementById('heading2').innerText = "World";
+    document.getElementById('heading2').style.color = "";
+    document.getElementById('input2').value = "";
 }
