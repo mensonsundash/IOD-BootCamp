c94c6172fcfbf920ff4208d670b84704a6c5344b
menso431 <menson.sundash@gmail.com>
2025-10-15 18:29:23 +1100

    Javascript Lab Exercise 3

---
+++ Module1/LabExercises/Exercise3/js/script.js
@@ -0,0 +1,11 @@
+function changeMe1() {
+    document.getElementById('column1').style.backgroundColor= "blue";
+    document.getElementById('heading1').innerText = "Hello Everyone";
+    document.getElementById('heading1').style.color = "#ffffff";
+}
+
+function changeMe2() {
+    document.getElementById('column2').style.backgroundColor= "red";
+    document.getElementById('heading2').innerText = "Welcome World";
+    document.getElementById('heading2').style.color = "#ffffff";
+}
