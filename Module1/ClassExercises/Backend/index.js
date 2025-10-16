b675c8d92046e70611b8a9bbe820230709f92464
menso431 <menson.sundash@gmail.com>
2025-10-16 21:08:41 +1100

    Node module sentiment added

--- Module1/ClassExercises/Backend/index.js
+++ Module1/ClassExercises/Backend/index.js
@@ -1,1 +1,4 @@
-console.log("hello all");
+const Sentiment = require('sentiment');
+const sentiment = new Sentiment();
+const result = sentiment.analyze('Cats are ok.');
+console.log(result); // Score: -2, Comparative: -0.666
