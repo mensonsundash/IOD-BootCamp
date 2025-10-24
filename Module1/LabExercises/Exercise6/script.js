5433bbd8981ee44f8686b50f6fa0d46311dcdcc1
menso431 <menson.sundash@gmail.com>
2025-10-24 14:18:08 +1100

    Lab Exercise 6 updated

--- Module1/LabExercises/Exercise6/script.js
+++ Module1/LabExercises/Exercise6/script.js
@@ -1,3 +1,4 @@
+//creating a JSON obect variable
 let book = {
                 title : "The Lord of the Rings",
                 description: "Percy Jackson is about to be kicked out of boarding school... again. And that's the least of his troubles. Lately, mythological monsters and the gods of Mount Olympus seem to be walking straight out of the pages of his Greek mythology textbook and into his life. Worse, he's angered a few of them. A lightning bolt has been stolen, and Percy is the prime suspect.",
@@ -5,7 +6,8 @@ let book = {
                 pageCount: 423
             }
 
-//print single data from object            
+//print individual data from object   
+console.log("********** Printing individual data from object **********"); 
 console.log("Title: ", book.title);
 console.log("Description: ", book.description);
 console.log("Author: ", book.author);
@@ -15,10 +17,12 @@ console.log("Number of pages: ", book.pageCount);
 console.log("Book Object: ", book);
 
 //book description updated
+console.log("********** Printing Updated description on object **********"); 
 book.description = "The first volume of J.R.R. Tolkien's epic high fantasy novel, The Lord of the Rings."
 console.log("Updated Book Object==> ", book);
 
 //creating 5 book object in array
+console.log("********** Creating 5 objects into a single array variable**********"); 
 let bookArray = [
   {
     title: "The Great Gatsby",
@@ -52,4 +56,5 @@ let bookArray = [
   }
 ];
 
+//printing the book array with 5 book objects
 console.log(bookArray[0])
