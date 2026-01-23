6485f5b005e0f56e11e9374b14abdf29fca89b8a
menso431 <menson.sundash@gmail.com>
2026-01-24 08:58:24 +1100

    M5 Lab Exercise6

---
+++ Module5/LabExercises/Exercise6/backend/controllers/calculateControllers.js
@@ -0,0 +1,36 @@
+const getData = (req, res) => {
+    res.send("Welcome to calculator")
+}
+const divide = (req, res) => {
+    const num1 = parseInt(req.query.num1);
+    const num2 = parseInt(req.query.num2);
+    let result = num1/num2;
+
+    res.status(200).json({data:result});
+}
+
+const multiply = (req, res) => {
+    const num1 = parseInt(req.query.num1);
+    const num2 = parseInt(req.query.num2);
+
+    let result = num1 * num2;
+    res.status(200).json({data:result});
+}
+
+const add = (req, res) => {
+    const number1= parseInt(req.query.num1);
+    const number2= parseInt(req.query.num2);
+    let result = number1 + number2;
+    
+    res.status(200).json({data:result})
+}
+
+const substract = (req, res) => {
+    const number1 = parseInt(req.query.num1);
+    const number2 = parseInt(req.query.num2);
+    let result = number1 - number2;
+
+    res.status(200).json({data: result});
+}
+
+module.exports = {getData, add, substract, multiply, divide}
