6c9d6ba5460312261f84d3b289e541a130b8d7f1
menso431 <menson.sundash@gmail.com>
2026-03-10 22:05:54 +1100

    M9 Class Exercise MongoDbapp

---
+++ Module9/CLassExercises/mongodbapp/server.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const app = express();
+
+require("dotenv").config();
+
+let dbConnect =  require("./dbConnect")
+
+const userRoutes = require("./routes/userRoutes");
+
+// parse requests of content-type - application/json
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.json({ message: "Welcome to my MongoDB application." });
+});
+
+app.use("/api/users", userRoutes); //will attach the routes to web server instance
+
+// set port, listen for requests
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}.`);
+});
