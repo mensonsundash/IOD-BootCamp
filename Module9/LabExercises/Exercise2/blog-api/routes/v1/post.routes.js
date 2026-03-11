d171d3f2b98a499824846943a0bc55945cc6624b
menso431 <menson.sundash@gmail.com>
2026-03-12 01:28:25 +1100

    m9:Lab2-Creating RESTful post API endpoint

--- Module9/LabExercises/Exercise2/blog-api/routes/v1/post.routes.js
+++ Module9/LabExercises/Exercise2/blog-api/routes/v1/post.routes.js
@@ -1,4 +1,17 @@
 const express = require("express");
 const app = express.Router();
 
+// importing controller functions available to connect routes
+const { postList, createPost, updatePost, deletePost } = require("../../controllers/post.controller")
+
+// app.get('/', (req, res) => {
+//     res.json({message: "This is router api V1: Post"});
+// })
+
+//RESTful API endpoints
+app.get("/", postList);
+app.post("/", createPost);
+app.put("/:id", updatePost);
+app.delete("/:id", deletePost);
+
 module.exports = app;
