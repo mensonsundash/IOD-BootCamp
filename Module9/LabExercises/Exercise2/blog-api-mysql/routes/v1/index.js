5280a0288b4b88f05ff44bb784775dcacffdb902
menso431 <menson.sundash@gmail.com>
2026-03-15 20:50:47 +1100

    m9:Lab3-created component routes

--- Module9/LabExercises/Exercise2/blog-api-mysql/routes/v1/index.js
+++ Module9/LabExercises/Exercise2/blog-api-mysql/routes/v1/index.js
@@ -5,5 +5,9 @@ app.get("/", (req, res) => {
     res.json({message: "Welcome to routes api: V1"});
 })
 
+app.use("/user", require("./user.routes")); // routes to user module
+app.use("/post", require("./post.routes")); // routes to post module
+app.use("/comment", require("./comment.routes")); // routes to comment module
+app.use("/like", require("./like.routes")) // routes to like module
 
 module.exports = app;
